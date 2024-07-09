80307ecb4629492c379440117c513ede64cc354a
angdan62 <angdan62@gmail.com>
2024-07-09 14:53:14 -0700

    CONTINUACION CURSO DE ALURA PARA REACT - HOOKS

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,47 +1,67 @@
-import styled from "styled-components"
-import Titulo from "../Titulo"
-import Populares from "./Populares"
-import Tag from "./Tags"
-import Imagen from "./Imagen"
+import styled from "styled-components";
+import Titulo from "../Titulo";
+import Populares from "./Populares";
+import Tag from "./Tags";
+import Imagen from "./Imagen";
 
 const GaleriaContainer = styled.div`
-display: flex;
-gap: 24px;
-`
+  display: flex;
+  gap: 24px;
+`;
 
 const SeccionFluida = styled.section`
-flex-grow: 1;
-`
+  flex-grow: 1;
+`;
 const ImagenesContainer = styled.section`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    gap: 24px;
-`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
 
+const Galeria = ({
+  fotos = [],
+  alSeleccionarFoto,
+  alAlternarFavorito,
+  consulta,
+}) => {
+  return (
+    <>
+      <Tag />
+      <GaleriaContainer>
+        <SeccionFluida>
+          <Titulo>Navegue por la galería</Titulo>
+          <ImagenesContainer>
+            {fotos
+              .filter((foto) => {
+                return (
+                  consulta == "" ||
+                  foto.titulo
+                    .toLocaleLowerCase()
+                    .normalize("NFD")
+                    .replace(/\p{Diacritic}/gu, "")
+                    .includes(
+                      consulta
+                        .toLocaleLowerCase()
+                        .normalize("NFD")
+                        .replace(/\p{Diacritic}/gu, "")
+                    )
+                );
+              })
+              .map((foto) => (
+                <Imagen
+                  alAlternarFavorito={alAlternarFavorito}
+                  alSolicitarZoom={alSeleccionarFoto}
+                  key={foto.id}
+                  foto={foto}
+                />
+              ))}
+          </ImagenesContainer>
+        </SeccionFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
+  );
+};
 
-const Galeria = ({ fotos = [], alSeleccionarFoto,alAlternarFavorito }) => {
-
-    return (
-        <>
-            <Tag />
-            <GaleriaContainer>
-                <SeccionFluida>
-                    <Titulo>Navegue por la galería</Titulo>
-                    <ImagenesContainer>
-                        {fotos.map(foto => <Imagen
-                        alAlternarFavorito= {alAlternarFavorito}
-                        alSolicitarZoom={alSeleccionarFoto}
-                            key={foto.id}
-                            foto={foto} />)
-                        }
-                    </ImagenesContainer>
-                </SeccionFluida>
-                <Populares />
-
-            </GaleriaContainer>
-        </>
-    )
-}
-
-export default Galeria
+export default Galeria;
